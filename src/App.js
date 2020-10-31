57340c16229011195e8edb28353e8f0feadb07d4
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-31 12:48:11 -0300

    Listando os repositórios do back-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from 'react';
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [repositoriesText, setRepositoriesText] = useState('Carregando repositórios...')
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    console.log(id)
   }
+
+  const listAllRepositories = useCallback(async () => {
+    try {
+      const getRepositories = await api.get('/repositories');
+      const { data } = getRepositories;
+
+      setRepositories(data);
+
+      if (!data) {
+        setRepositoriesText('Nenhum repositório encontrado');
+      }
+
+    } catch (error) {
+      setRepositoriesText('Tivemos um problema ao carregar os repositórios');
+    }
+  }, []);
+
+  useEffect(() => {
+    listAllRepositories();
+  }, [listAllRepositories]);
+
+  const listRepositories = repositories.length > 0 ? (
+    repositories.map(repo => (
+      <li key={repo.id}>
+        { repo.title }
+        <button onClick={() => handleRemoveRepository(repo.id)}>
+          Remover
+        </button>
+      </li>
+    ))) : <li>{ repositoriesText }</li>; 
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { listRepositories }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
